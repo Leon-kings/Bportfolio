f11e8e58fed1b642ea079401ae1d5862555b6e33
Leon-kings <leonakingeneye2002@gmail.com>
2025-02-26 18:41:27 -0800

    changes on server port line and line gathered

--- Server.js
+++ Server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   res.status(200).json({ status: 'success', message: 'Welcome to my API' });
 });
 
-app.use('/messages', Message);
+app.use('/0998C28', Message);
 
 // Start the server
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
