3bb8bce24a7f98ac54f8ae1d64e99b424dc4b9a2
Leon-kings <leonakingeneye2002@gmail.com>
2025-03-07 16:54:36 -0800

    app.use()

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const Message = require('./routes/messageRoutes.js');
 
 dotenv.config();
-app.use()
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
